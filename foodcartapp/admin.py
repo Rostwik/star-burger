989857a61e435ae60a5828282ef8a616721e57d9
Rostislav <rostwik@gmail.com>
2023-07-11 17:44:38 +0300

    Add models for order data.

--- foodcartapp/admin.py
+++ foodcartapp/admin.py
@@ -3,7 +3,7 @@ from django.shortcuts import reverse
 from django.templatetags.static import static
 from django.utils.html import format_html
 
-from .models import Product
+from .models import Product, Order, OrderItems
 from .models import ProductCategory
 from .models import Restaurant
 from .models import RestaurantMenuItem
@@ -12,6 +12,18 @@ from .models import RestaurantMenuItem
 class RestaurantMenuItemInline(admin.TabularInline):
     model = RestaurantMenuItem
     extra = 0
+
+
+class OrderItemsInline(admin.TabularInline):
+    model = OrderItems
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'firstname', 'lastname', 'phone_number', 'address')
+    inlines = [
+        OrderItemsInline,
+    ]
 
 
 @admin.register(Restaurant)
@@ -91,13 +103,16 @@ class ProductAdmin(admin.ModelAdmin):
         if not obj.image:
             return 'выберите картинку'
         return format_html('<img src="{url}" style="max-height: 200px;"/>', url=obj.image.url)
+
     get_image_preview.short_description = 'превью'
 
     def get_image_list_preview(self, obj):
         if not obj.image or not obj.id:
             return 'нет картинки'
         edit_url = reverse('admin:foodcartapp_product_change', args=(obj.id,))
-        return format_html('<a href="{edit_url}"><img src="{src}" style="max-height: 50px;"/></a>', edit_url=edit_url, src=obj.image.url)
+        return format_html('<a href="{edit_url}"><img src="{src}" style="max-height: 50px;"/></a>', edit_url=edit_url,
+                           src=obj.image.url)
+
     get_image_list_preview.short_description = 'превью'
 
 
