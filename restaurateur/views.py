cb3d9d2ed275fa80ef110e13da7d4e9b7a9c8528
Rostislav <rostwik@gmail.com>
2023-07-17 15:25:36 +0300

    Added link on admin order editing

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -93,6 +93,6 @@ def view_restaurants(request):
 @user_passes_test(is_manager, login_url='restaurateur:login')
 def view_orders(request):
     orders = list(Order.objects.all().calculate_order_sum())
-    return render(request, template_name='order_items.html', context={
-        "order_items": orders
+    return render(request, template_name='orders.html', context={
+        "orders": orders
     })
