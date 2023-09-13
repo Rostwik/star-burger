4ed9bf7bee949298e1830203401d690f694d6cda
Rostislav <rostwik@gmail.com>
2023-09-13 20:27:54 +0300

    Refactoring

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -8,6 +8,6 @@ npm ci --dev
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
-systemctl daemon-reload
+systemctl reload nginx
 curl -H "X-Rollbar-Access-Token: $(cat .env | grep ROLLBAR_ACCESS_TOKEN| cut -d "=" -f 2)" -H "Content-Type: application/json" -X POST 'https://api.rollbar.com/api/1/deploy' -d '{"environment": "development", "revision": "'"$(git rev-parse HEAD)"'", "rollbar_name": "rostwik", "local_username": "rostwik", "status": "succeeded"}'
 echo "Деплой успешно завершен"
