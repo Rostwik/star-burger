76fc300edc5e6f8f2b3baa0970ea666f003f3a69
Rostislav <rostwik@gmail.com>
2023-09-13 20:26:25 +0300

    Refactoring

--- deploy_star_burger.sh
+++ deploy_star_burger.sh
@@ -5,6 +5,7 @@ cd /opt/Star-burger/star-burger
 git pull
 pip install -r requirements.txt
 npm ci --dev
+./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
 systemctl daemon-reload
